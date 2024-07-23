276364795705209697bf69aeeabbb87d76783f71
CHAOUCH Donia <151736149+Doniach28@users.noreply.github.com>
2024-07-23 11:14:47 +0100

    Update res_partner.py dirigeant1

--- ad_api_pappers/models/res_partner.py
+++ ad_api_pappers/models/res_partner.py
@@ -93,10 +93,10 @@ class ResPartner(models.Model):
                 adresse_ligne_1 = data['adresse_ligne_1']
                 self.write({'street': adresse_ligne_1})
                 _logger.info(f"street updated to: {adresse_ligne_1}")
-            if 'prenom' in data:
-                prenom = data['prenom']
-                self.write({'x_dirigeants': prenom})
-                _logger.info(f"Dirigeants   updated to: {prenom}")
+            if 'nom_complet' in data:
+                nom_complet = data['nom_complet']
+                self.write({'x_dirigeants': nom_complet})
+                _logger.info(f"Dirigeants   updated to: {nom_complet}")
 
 
         except requests.exceptions.RequestException as e:
